fb1f617eba3d007e28fa604a3e6ebd6b10ded44d
Prasoon Mahawar <prasoonmahawar21@gmail.com>
2025-11-20 20:52:11 +0530

    activity 1 and 2

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,14 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn first_name() {
+    println!("Prasoon");
+}
+
+fn last_name(){
+    println!("Mahawar");
+}
+fn main() {
+    first_name();
+    last_name();
+}
--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,14 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add_numbers(a: i32, b:i32) -> i32 {
+    a+b
+}
+
+fn display_result(result: i32) {
+    println!("The sum of the two numbers is: {:?}", result);
+}
+fn main() {
+    let sum = add_numbers(5, 10);
+    display_result(sum);
+}
