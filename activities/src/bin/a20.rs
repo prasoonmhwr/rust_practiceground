b83dd02ea05397dd071eeee17dbb83ebace25931
Prasoon Mahawar <prasoonmahawar21@gmail.com>
2025-11-27 17:47:59 +0530

    activity 20 and project 1

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -22,5 +22,46 @@
 // * Use a match expression to convert the user input into the power state enum
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
-
-fn main() {}
+use std::io;
+enum State{
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+impl State{
+    fn new(state: &str) -> Option<State>{
+        let state = state.trim().to_lowercase();
+        match state.as_str(){
+            "off" => Some(State::Off),
+            "sleep" => Some(State::Sleep),
+            "reboot" => Some(State::Reboot),
+            "shutdown" => Some(State::Shutdown),
+            "hibernate" => Some(State::Hibernate),
+            _ => None
+        }
+    }
+}
+fn take_action(state: State){
+    use State::*;
+    match state{
+        Off => println!("turning off"),
+        Sleep => println!("sleeping"),
+        Reboot => println!("rebooting"),
+        Shutdown => println!("shutting down"),
+        Hibernate => println!("hibernating"),
+    }
+}
+fn main() {
+    let mut buffer = String::new();
+    let user_input_status = io::stdin().read_line(&mut buffer);
+    if user_input_status.is_ok(){
+        match State::new(&buffer){
+            Some(state) => take_action(state),
+            None => println!("invalid Power state"),
+        }
+    } else {
+        println!("error reading input");
+    }
+}
--- activities/src/bin/p1.rs
+++ activities/src/bin/p1.rs
@@ -17,5 +17,159 @@
 //   throughout your program.
 // * Create your program starting at level 1. Once finished, advance to the
 //   next level.
+use std::io;
+use std::collections::HashMap;
 
-fn main() {}
+#[derive(Debug, Clone)]
+struct Bill{
+    name: String,
+    amount: f64
+}
+
+struct Bills {
+    inner: HashMap<String, Bill>
+}
+
+impl Bills {
+    fn new() -> Self{
+        Self {inner: HashMap::new()}
+    }
+
+    fn add(&mut self,bill: Bill){
+        self.inner.insert(bill.name.clone(),bill);
+    }
+
+    fn get_all(&self) -> Vec<Bill> {
+        let mut bills = vec![];
+        for bill in self.inner.values(){
+            bills.push(bill.clone());
+        }
+        bills
+    }
+
+    fn remove(&mut self, name: &str) -> bool{
+        self.inner.remove(name).is_some()
+    }
+
+    fn update(&mut self, name: &str, amount: f64) -> bool {
+        match self.inner.get_mut(name) {
+            Some(bill) => {
+                bill.amount = amount;
+                true
+            }
+            None => false,
+        }
+    }
+}
+fn read_input() -> Option<String>{
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err(){
+        println!("Please enter your data again")
+    } 
+    let input = buffer.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+fn get_bill_amount() -> Option<f64>{
+    println!("Amount:");
+    loop {
+        let input = match read_input(){
+            Some(input) => input,
+            None => return None,
+        };
+        if &input == "" {
+            return None;
+        }
+        let parsed_input: Result<f64,_> = input.parse();
+        match parsed_input {
+            Ok(amount) => return Some(amount),
+            Err(_) => println!("Please enter a number"),
+        }
+    }
+}
+fn add_bill_menu(bills: &mut Bills){
+    let name = match read_input(){
+        Some(input) => input,
+        None => return,
+    };
+    let amount = match get_bill_amount(){
+         Some(amount) => amount,
+         None => return,
+    };
+    let bill = Bill {name, amount};
+    bills.add(bill);
+    println!("Bill added");
+}
+fn remove_bill_menu(bills: &mut Bills){
+    for bill in bills.get_all(){
+        println!("{:?}",bill);
+    }
+    let input = match read_input(){
+        Some(input) => input,
+        None => return,
+    };
+    if bills.remove(&input){
+        println!("removed");
+    }else {
+        println!("bill not found");
+    }
+}
+fn update_bill_menu(bills: &mut Bills){
+    for bill in bills.get_all(){
+        println!("{:?}",bill);
+    }
+    println!("Enter bill name to udpate:");
+    let input = match read_input(){
+        Some(input) => input,
+        None => return,
+    };
+    let amount = match get_bill_amount(){
+         Some(amount) => amount,
+         None => return,
+    };
+    if bills.update(&input, amount){
+        println!("updated");
+    }else {
+        println!("bill not found");
+    }
+}
+fn view_bills_menu(bills: &Bills){
+    for bill in bills.get_all(){
+        println!("{:?}", bill);
+    }
+}
+fn main_menu(){
+    fn show() {
+        println!("");
+        println!("== Manage Bills ==");
+        println!("1. Add bill");
+        println!("2. View bills");
+        println!("3. Remove bill");
+        println!("4. Update bill");
+        println!("");
+        println!("Enter");
+    }
+
+    let mut bills = Bills::new();
+
+    loop {
+        show();
+        let input = match read_input(){
+        Some(input) => input,
+        None => return,
+    };
+        match input.as_str(){
+            "1" => add_bill_menu(&mut bills),
+            "2" => view_bills_menu(&bills),
+            "3" => remove_bill_menu(&mut bills),
+            "4" => update_bill_menu(&mut bills),
+            _ => break,
+        }
+    }
+}
+fn main() {
+    main_menu();
+}
